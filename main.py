f131bfa97e359eb66b49c23c60485bee6b38cac7
Elchin <>
2018-08-03 16:34:53 +0300

    Task 2 3

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import threading
 import multiprocessing
 import sys
+import time
+from functools import partial
+import requests
+
 from tailrecursion import tailrec
 from timer import timer
 
@@ -17,19 +21,33 @@ def fact(n):
     return tail_fact(n)
 
 
-@timer
-def seq_fun(callback, amount):
-    while amount > 0:
-        callback()
-        amount -= 1
+def time_delay(delay=1):
+    time.sleep(1)
 
 
 @timer
-def thread_fun(callback, amount):
+def seq_fun(callback, amount=None):
+    if amount:
+        while amount > 0:
+            callback()
+            amount -= 1
+    else:
+        for call in callback:
+            call()
+
+
+@timer
+def thread_fun(callback, amount=None):
     threads = []
-    while amount > 0:
-        threads.append(threading.Thread(target=callback))
-        amount -= 1
+
+    if amount:
+        while amount > 0:
+            threads.append(threading.Thread(target=callback))
+            amount -= 1
+    else:
+        for call in callback:
+            threads.append(threading.Thread(target=call))
+
     for i in threads:
         i.start()
     for i in threads:
@@ -37,21 +55,50 @@ def thread_fun(callback, amount):
 
 
 @timer
-def proc_fun(callback, amount):
+def proc_fun(callback, amount=None):
     processes = []
-    while amount > 0:
-        processes.append(multiprocessing.Process(target=callback))
-        amount -= 1
+
+    if amount:
+        while amount > 0:
+            processes.append(multiprocessing.Process(target=callback))
+            amount -= 1
+    else:
+        for call in callback:
+            processes.append(multiprocessing.Process(target=call))
+
     for i in processes:
         i.start()
     for i in processes:
         i.join()
+
+
+@timer
+def download_video(url):
+    name = url.split('/')[-1]
+    print(name)
+    content = requests.get(url)
+    with open(name, 'wb') as file:
+        file.write(content.content)
 
 
 if __name__ == '__main__':
     print(sys.version)
+
     N = 60000
     CPU_CORES_AMOUNT = multiprocessing.cpu_count() + 1
-    seq_fun(lambda: fact(N), CPU_CORES_AMOUNT)
-    thread_fun(lambda: fact(N), CPU_CORES_AMOUNT)
-    proc_fun(lambda: fact(N), CPU_CORES_AMOUNT)
+    #callback = lambda: fact(N, CPU_CORES_AMOUNT)
+    callback = time_delay
+    seq_fun(callback, CPU_CORES_AMOUNT)
+    thread_fun(callback, CPU_CORES_AMOUNT)
+    proc_fun(callback, CPU_CORES_AMOUNT)
+
+    VIDEO_URLS = [
+        'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_2mb.mp4',
+        'http://www.sample-videos.com/video/mp4/480/big_buck_bunny_480p_2mb.mp4',
+    ]
+
+    download_functions = [partial(download_video, url) for url in VIDEO_URLS]
+    seq_fun(download_functions)
+    thread_fun(download_functions)
+    proc_fun(download_functions)
+
--- timer.py
+++ timer.py
@@ -5,7 +5,7 @@ from functools import wraps
 def timer(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print('^^^Start {}^^^'.format(func.__name__))
+        print('+++Start {}+++'.format(func.__name__))
         start = time.time()
         res = func(*args, **kwargs)
         print("---Finish {} execution time = {}---".format(func.__name__,
